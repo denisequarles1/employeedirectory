cf44a2c6d0278baf83cdf512857fb35d303e73f7
denisequarles1 <denise.quarles1@gmail.com>
2021-02-15 01:46:30 -0500

    updated my-app folder

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+function Footer(){
+    return (
+        <footer>
+            <ul className="footer">
+                <li>Employee Directory</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
